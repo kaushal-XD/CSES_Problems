3be490dc8e80bc2dc08e549bf4ff587342345cbb
Kaushal Landge <er.kaushallandge@gmail.com>
2026-07-10 10:25:18 +0530

    Add C++ solution for Coin_Combinations_II

---
+++ Dynamic_Programming/Coin_Combinations_II/Coin_Combinations_II.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem Name: Coin_Combinations_II
+ * Language: C++
+ * Category: Dynamic_Programming
+ * Date: 2026-07-10
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  
+ 
+    int n,x;
+    
+    cin>>n>>x;
+    vector<int> arr(n);
+    for(int i = 0 ; i < n ; i ++) {
+        cin >> arr[i];
+    }
+    int mod = 1e9 + 7;
+ 
+    vector<int> dp(x+1,0);
+    dp[0] = 1;
+    for (int c : arr){
+        for(int i = 1; i < x+1; i++){
+            if (i-c >= 0) 
+                dp[i] = (dp[i] + dp[i-c])%mod;
+        }
+    }
+    cout<<dp[x]<<"\n";
+    return 0;
+}
