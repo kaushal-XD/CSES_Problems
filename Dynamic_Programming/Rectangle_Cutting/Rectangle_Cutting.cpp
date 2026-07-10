34ce1f6f59ff285792f2cf8dce0c644f808c02b9
Kaushal Landge <er.kaushallandge@gmail.com>
2026-07-11 00:24:56 +0530

    Add C++ solution for Rectangle_Cutting

---
+++ Dynamic_Programming/Rectangle_Cutting/Rectangle_Cutting.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem Name: Rectangle_Cutting
+ * Language: C++
+ * Category: Dynamic_Programming
+ * Date: 2026-07-10
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n , m ;
+    cin>>n >> m ;
+ 
+    vector<vector<int>> dp(n+1,vector<int>(m+1,1e9));
+ 
+    for (int i = 1; i < n+1 ; i++){
+        for (int j = 1 ; j < m+1 ; j++){
+            if (i == j ) dp[i][j] = 0 ; 
+ 
+            else {
+                for(int v = 1 ; v <= i-1 ; v++){
+                    dp[i][j] = max(dp[i][j],dp[j-v][j] + dp[v][j] + 1);
+                }
+                for(int h = 1 ; h <= j-1 ; h++){
+                    dp[i][j] = max(dp[i][j],dp[i][h] + dp[i][j-h] + 1);
+                }
+ 
+            }
+        
+        }
+    }
+    cout<<dp[n][m];
+    return 0;
+ 
+}
