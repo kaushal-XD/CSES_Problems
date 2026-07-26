45c1e3f6a149dc428ad6a99231f08a16822e92e5
Kaushal Landge <er.kaushallandge@gmail.com>
2026-07-26 15:48:10 +0530

    Add C++ solution for Building_Roads

--- Graph_Algorithms/Building_Roads/Building_Roads.cpp
+++ Graph_Algorithms/Building_Roads/Building_Roads.cpp
@@ -5,7 +5,49 @@
  * Date: 2026-07-26
  */
 
-input/code.cpp: In function 'int main()':
-input/code.cpp:22:13: warning: right operand of comma operator has no effect [-Wunused-value]
-   22 |     cin>>n,m;
-      |             ^
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+vector<vector<int>> adj;
+vector<bool> visited;
+ 
+ 
+void dfs( int u){
+    visited[u] = true;
+    for(int v : adj[u]){
+        if (!visited[v]){
+            dfs(v);
+        }
+    }
+}
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    int n,m;
+    cin>>n>>m;
+    adj.resize(n+1);
+    visited.assign(n+1,false);
+ 
+    for(int i = 0 ; i < n ; i++){
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    vector<int> leads;
+    for(int i = 1 ; i <= n ; i++){
+        if (!visited[i]){
+            leads.push_back(i);
+            dfs(i);
+        }
+    }
+ 
+    int k = leads.size()-1;
+    cout<<k<<"\n";
+    for(int i = 0 ; i < k ; i++){
+        cout<<leads[i]<<" "<<leads[i+1]<<"\n";
+    }
+    return 0;
+}
