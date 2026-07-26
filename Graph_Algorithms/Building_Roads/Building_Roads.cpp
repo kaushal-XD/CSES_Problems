ba43dfb6f0d43990ca1b571bf3d443c345d3ce23
Kaushal Landge <er.kaushallandge@gmail.com>
2026-07-26 15:47:28 +0530

    Add C++ solution for Building_Roads

---
+++ Graph_Algorithms/Building_Roads/Building_Roads.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Building_Roads
+ * Language: C++
+ * Category: Graph_Algorithms
+ * Date: 2026-07-26
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:22:13: warning: right operand of comma operator has no effect [-Wunused-value]
+   22 |     cin>>n,m;
+      |             ^
